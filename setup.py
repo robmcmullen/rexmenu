0232ccc6b1ba1066f36ceeb5d1c6e6620f8c081e
Rob McMullen <feedback@playermissile.com>
2017-01-09 13:39:34 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+scripts = ["rexmenu.py"]
+if os.path.exists("/usr/bin/tvservice"):
+    scripts.append("scripts/rpi-screen-blank.py")
+
+setup(name='rexmenu',
+      version='1.0',
+      description='Simple frontend for MAME',
+      author='Rob McMullen',
+      author_email='feedback@playermissile.com',
+      url='https://playermissile.com',
+      scripts=scripts,
+     )
