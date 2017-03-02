b42b37dfdf4e637eab2582314daf152327e9c61b
Rob McMullen <feedback@playermissile.com>
2017-03-02 11:00:33 -0800

    Updated setup.py in preparation for PyPI

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ scripts = ["rexmenu.py"]
 if os.path.exists("/usr/bin/tvservice"):
     scripts.append("scripts/rpi-screen-blank.py")
 
+with open("README.rst", "r") as fp:
+    long_description = fp.read()
+print long_description
+
 setup(name='rexmenu',
       version='2.0',
-      description='Simple frontend for MAME',
+      description='Simple image tiling program launcher/MAME frontend using Pygame',
+      long_description=long_description,
       author='Rob McMullen',
       author_email='feedback@playermissile.com',
       url='https://playermissile.com',
       scripts=scripts,
+      license="GPL",
+      install_requires = [
+          'pygame',
+          ],
      )
